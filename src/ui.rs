402bce7754b31d07412b91fd0f2d13655b72b96e
Sandesh Grangdan <sandesh.tamang@innovatetech.co>
2024-03-25 19:37:39 +0545

    Integrated AWS SDK.

---
+++ src/ui.rs
@@ -0,0 +1,50 @@
+use ratatui::{
+    prelude::{Alignment, Frame},
+    style::{Color, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
+};
+
+use crate::app::App;
+
+pub fn render(app: &mut App, f: &mut Frame) {
+    f.render_widget(
+        Paragraph::new(format!(
+            "
+        Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+        Press `j` and `k` to increment and decrement the counter respectively.\n\
+        Counter: {}
+      ",
+            app.counter
+        ))
+        .block(
+            Block::default()
+                .title("AWS (SSM)")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Yellow))
+        .alignment(Alignment::Center),
+        f.size(),
+    );
+    f.render_widget(
+      Paragraph::new(format!(
+          "
+      Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+      Press `j` and `k` to increment and decrement the counter respectively.\n\
+      Counter: {}
+    ",
+          app.counter
+      ))
+      .block(
+          Block::default()
+              .title("AWS (SSM)")
+              .title_alignment(Alignment::Center)
+              .borders(Borders::ALL)
+              .border_type(BorderType::Rounded),
+      )
+      .style(Style::default().fg(Color::Yellow))
+      .alignment(Alignment::Center),
+      f.size(),
+  )
+}
